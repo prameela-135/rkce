58c71169a83770ccd6ec407cdc7e2650d05f46a9
prameela-135 <prameelabandikatla@gmail.com>
2024-10-03 22:33:44 -0700

    Add files via upload

---
+++ interface.java
@@ -0,0 +1,22 @@
+interface printable{
+    void print();
+    interface showable{
+        vode show();
+        class A7  implements printable,showable{
+            public void print(){
+                system.out.println("H ello");
+                
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+      \ MOUNIhghghghghhuyguyguyg6tuybhbkgtdfr676ybhbgv65r67yunn hgcfgcfvjhhbk\\[]014024];kiy
+    }''
